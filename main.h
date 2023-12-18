f01418ee7afea8ade8af4d801cfeea42c62ecf71
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2023-12-18 14:55:15 +0100

    Add main.h and update of b.c for betty check

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+
+
+
+
+
+#endif /*HEADER_H*/
+
