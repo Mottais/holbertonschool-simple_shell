800c58401f603f3769dc6ff0242a5e3b67b23776
Mottais <mottais.afy@outlook.fr>
2023-12-21 01:08:40 -0800

    Gestion des erreurs et fuites de mémoire

--- b.c
+++ b.c
@@ -1,4 +1,3 @@
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,66 +12,101 @@
 /**
  * args_dans_tableau - function that prints '$' to standard output
  *
- * @input: -
- * @args: -
- * Return:
+ * @user_input: -
+ * Return: tableau d'argument de l'input
  */
 /* Fonction pour diviser la ligne en commandes et arguments*/
-void args_dans_tableau(char *input, char *args[])
+char **args_dans_tableau(char *user_input)
 {
-	char *jeton = strtok(input, " \n\t");
-	int i = 0;
+	const char *chaineEntree = user_input;
+	int nbMots = 0, i;
+	char **mots;
+	char *copieChaine = strdup(chaineEntree);
+	char *jeton = strtok(copieChaine, " \n\t");
 
-	while (jeton != NULL && i < MAX_ARGS - 1)
+	while (jeton != NULL)
 	{
-		args[i++] = jeton;
+		nbMots++;
 		jeton = strtok(NULL, " \n\t");
 	}
 
-	args[i] = NULL;  /* Ajout arg NULL pour la fonction execve */
+	mots = malloc((nbMots + 1) * sizeof(char *));
+
+	free(copieChaine);
+	copieChaine = strdup(chaineEntree);
+
+	jeton = strtok(copieChaine, " ");
+
+	i = 0;
+	while (jeton != NULL)
+	{
+		mots[i] = strdup(jeton);
+		i++;
+		jeton = strtok(NULL, " ");
+	}
+	mots[i] = NULL;
+
+	free(copieChaine);
+
+	return (mots);
 }
 
 
 /**
  * execute_command - function that prints '$' to standard output
  *
- * @env: -
- * @cmd: -
- * @args: -
+ * @cmd_trouv: =1 si args[0] est un éxé, 0 sinon
+ * @args: - tableau des arguements
+ * @env: environement
+ * @count_cmd: -
+ * @av_0: - nom de la fonctio, exemple : ./hsh
  * Return: status
  */
 /* Fonction pour créer un processus enfant et exécuter la commande */
-int execute_command(char *cmd, char *args[], char **env)
+int execute_command(int cmd_trouv, char *args[], char **env,
+			int count_cmd, char *av_0)
 {
 	pid_t pid;
-	int status;
+	int status = 0;
 
-	pid = fork(); /* création processus enfant */
-
-	/* si création processus enfant echoue */
-	if (pid == -1)
+	if (cmd_trouv == 0) /* si aucun éxé trouvé */
 	{
-		perror("Erreur lors du fork\n");
-		return (EXIT_FAILURE);
+		/* msg err */
+		fprintf(stderr, "%s: %d: %s: not found\n", av_0, count_cmd, args[0]);
+		return (127); /* retour code err */
 	}
-
-	/* processus enfant */
-	if (pid == 0)
-	{/* Exec cmd avec les arguments puis si réeussit,ferme le processus enfant */
-		if (execve(cmd, args, env) == -1)
-	{/*Si Exec echoue imprime message erreur puis ferme le processus enfant */
-		perror("execve");
-		exit(EXIT_FAILURE);
-	}
-	}
-
-	/* processus enfant */
 	else
-	wait(&status);/*Attend fin process enfant et reccup status processus enfant*/
+	{
+		pid = fork(); /* création processus enfant */
 
-	return (status);
+		/* si création processus enfant echoue */
+		if (pid == -1)
+		{
+			perror("Erreur lors du fork\n");
+			return (EXIT_FAILURE);
+		}
+		/* processus enfant */
+		if (pid == 0)
+		{
+			/* Exec cmd avec les arguments puis si réeussit,ferme le processus */
+			if (execve(args[0], args, env) == -1)
+			{
+				/*Si Exec echoue, imprime msg err et ferme processus enfant */
+				fprintf(stderr, "%s: %d: %s: not found\n", av_0, count_cmd, args[0]);
+				exit(127);
+			}
+		}
+		/* processus parent */
+		else if (pid > 0)
+		{
+			wait(&status); /*Attend fin process enfant et reccup status son processus */
+			if ((WEXITSTATUS(status)) != 0)  /*si status de processus enfant non null */
+				status = (WEXITSTATUS(status)); /*réccup status  processus enfant */
+			return (status);
+		}
+	}
+	return (0);
 }
-
 
 /**
  * _getenv - function that prints '$' to standard output
@@ -86,51 +120,83 @@ char *_getenv(const char *name, char **env)
 	if (name == NULL)
 		return (NULL);
 
-	for (; *env != NULL; ++env)
+	for (; *env != NULL; ++env) /* boucle dans le tableau des variablesq d'env */
 	{
-	if (strncmp(*env, name, strlen(name)) == 0)/*&& (*env)[strlen(name)] == '=')*/
-	{
-		return (*env + strlen(name) + 1);
-	}
+		/* si début = valeur cherchée (name) + suivi de carac '=' */
+		if (strncmp(*env, name, strlen(name)) == 0 && (*env)[strlen(name)] == '=')
+		{
+			return (*env + strlen(name) + 1); /* revoi variable name apres le '=' */
+		}
 	}
 
-	return (NULL);
+	return (NULL); /* si pas trouvée la variable name */
 }
 
 /**
  * _which - function that prints '$' to standard output
  *
  * @env: -
- * @cmd: -
+ * @args: -
  * Return: NULL
  */
 
-char *_which(const char *cmd, char **env)
+
+int _which(char **args, char **env)
 {
 	char *path, *path_copy, *jeton;
-	char full_path[256];
+	char full_path[MAX_ARGS];
 	struct stat file_stat;
 
-	path = _getenv("PATH", env);
-	path_copy = strdup(path);
+	/* si arg[0] (commande) est un éxé (full_path/file) */
+	if ((stat(args[0], &file_stat) == 0) && S_ISREG(file_stat.st_mode))
+		return (1); /* un éxé a été trouvé */
+
+	path = _getenv("PATH", env); /* reccup variable PATH de l'environnement */
+				    /* qui est une de chemins séparés par des ':'*/
+
+	path_copy = strdup(path); /* duplication path  accéssible en écriture */
 
 	jeton = strtok(path_copy, ":");
+	/* jeton pointe sur debut de path_copy */
+	/* et remplace le 1er caractère ":" par le caract de fin : '\0' */
 
-	while (jeton != NULL)
+	while (jeton != NULL) /* jusqu'à la fin de path_copy */
 	{
-		snprintf(full_path, sizeof(full_path), "%s/%s", jeton, cmd);
+		/* full_path = concatenation de jeton + '/' + arg[0] (commande) */
+		snprintf(full_path, sizeof(full_path), "%s/%s", jeton, args[0]);
 
+		/* si full_path est un éxé */
 		if (stat(full_path, &file_stat) == 0 && S_ISREG(file_stat.st_mode))
 		{
+
 			free(path_copy);
-			return (strdup(full_path));
+			free(args[0]);
+			args[0] = strdup(full_path); /* modifie arg[0] avec full_path */
+			return (1); /* un éxé a été trouvé */
 		}
 
 		jeton = strtok(NULL, ":");
+		/* jeton pointe sur le path suiant */
+		/* et remplace le prochain caractère ":" par le caract de fin : '\0' */
 	}
-
 	free(path_copy);
-	return (NULL);
+
+	return (0); /* pas d'éxé trouvé */
+}
+
+/**
+ * libererMemoireTab - libere la mémoire d'un tableau
+ *
+ * @args: -
+ */
+void libererMemoireTab(char **args)
+{
+	int i;
+
+	for (i = 0; args[i] != NULL; i++)
+	free(args[i]);
+
+	free(args);
 }
 
 /**
@@ -141,52 +207,46 @@ char *_which(const char *cmd, char **env)
  * @env: -
  * Return: status
  */
-
-
-
 int main(int ac, char **av, char **env)
 {
-	char *user_input = NULL, *full_path, *args[MAX_ARGS];
-	size_t bufsize = 0, length;
-	int status = 0;
+	char *user_input = NULL, **args;
+	size_t bufsize = 0;
+	ssize_t  length;
+	int  count_cmd = 0, cmd_trouv, status = 0;
 	(void)ac, (void)av;
 
-	if (isatty(STDIN_FILENO))
-		printf("$  "); /* affiche le prompt */
+	if (isatty(STDIN_FILENO)) /* si mode interractif */
+		printf("$ "); /* affiche le prompt */
 
-	while (getline(&user_input, &bufsize, stdin) != -1)/*attend saisie user */
+	/* input utilisateur : saisie clavier si ineterractif sinon input standard */
+	while ((length = getline(&user_input, &bufsize, stdin)) != -1)
 	{
-		length = strlen(user_input);
-
-		/* remplace le caractère de saut de ligne par caractere null */
-		/* if (length > 0 && user_input[length - 1] == '\n')*/
-		user_input[length - 1] = '\0';
+		user_input[length - 1] = '\0'; /* remplace dernier caractère par */
 
 		if (length > 1)
 		{
-			/* cré un tableau avec chaque mots de la saisie utilisateur*/
-			args_dans_tableau(user_input, args);
-			/* si commande = "exit"*/
-			if (strcmp(args[0], "exit") == 0)
+			count_cmd++; /*compte nb de fois qu'une commande est éxé (pour msg err) */
+			args = args_dans_tableau(user_input);/*créer tableau avec args de input*/
+
+			if (strcmp(args[0], "exit") == 0)   /*si commande (args[0])= "exit" */
 			{
-				status = 1;
-				break;
+				libererMemoireTab(args); /* Libérer la mémoire allouée au tab args  */
+				break;  /*quitte la boucle pour quitter le programme */
 			}
-			full_path = _which(args[0], env);/*teste si args[0] existe dans env*/
-			if (full_path)
-			{
-				status = execute_command(full_path, args, env);
-				free(full_path);
-			}
-			else
-			status = execute_command(args[0], args, env);
+			/*teste si args[0] existe dans env et complete arg[0] avec path si besoin*/
+			cmd_trouv = _which(args, env);
+
+			/* Exceute commande et retourn code erreur le cas echeant*/
+			status = execute_command(cmd_trouv, args, env, count_cmd, av[0]);
+
+			/* Libérer la mémoire allouée a args  */
+			libererMemoireTab(args);
 		}
-		if (isatty(STDIN_FILENO))
-		printf("$ "); /* affiche le prompt */
-	}
-	free(user_input);
+
+		if (isatty(STDIN_FILENO)) /* si mode interractif */
+			printf("$ "); /* affiche le prompt */
+
+	} /*fin while*/
+	free(user_input); /* libere la mémore issue de la fonction getline */
 	return (status);
 }
-
-
-
