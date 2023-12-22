f43d6101ccc0fd16ca1c1663e89f13dd4046ce98
Mottais <mottais.afy@outlook.fr>
2023-12-22 09:50:15 -0800

    Test3

--- simple_shell.c
+++ simple_shell.c
@@ -1,218 +1,82 @@
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
+#include "main.h"
+/**
+ * _print_env - imprime les varaibles d'environnement
+ * @env: Tableau des variables d'environnement et leur valeur
+ */
+void _print_env(char **env)
+{
+	int i = 0;
 
-void print_prompt(void);
+	while (env[i])
+		printf("%s\n", env[i++]);
+}
 
-int exec_cmd(char *command, char **arguments, char **env, char
-**argv, int *count_command);
 
-char **parse_command_line(char *command, char **arguments);
-void free_args(char **arguments);
-char *get_path(char *command, char **env);
 
 /**
- * exec_cmd - execute a command
- * @command: the command to execute
- * @arguments: array of pointers to arguments
- * @env: environment variables
- * @argv: arguments of the ./simple shell
- * @count_command: counts the number of commands executed
+ * est_vide - controle si la chaine est vide ([espace] considéré vide)
  *
- * Description: create a child to execute the command
+ *@user_input: chaine à controler
+ * Return: 1 si vide 0 sinon
+ */
+int est_vide(char *user_input)
+{
+	int i = 0;
+
+	while (user_input[i] == ' ')
+		i++;
+
+	return (user_input[i] == 0);
+}
+
+/**
+ * main - simple shell
+ *
+ * @ac: nombre d'argument (pas utilisé)
+ * @av: tableau d'argumet du programme simple shell
+ * @env: Tableau des variables d'environnement et leur valeur
  * Return: status
  */
-int exec_cmd(char *command, char **arguments, char **env,
-char **argv, int *count_command)
+int main(int ac, char **av, char **env)
 {
-	pid_t pid;
-	int status = 0;
-	/*struct stat file_stats;*/
+	char *user_input = NULL, **args;
+	size_t bufsize = 0;
+	ssize_t  length;
+	int  count_cmd = 0, cmd_trouv, status = 0;
+	(void)ac, (void)av;
 
-	if (command != NULL)
+	if (isatty(STDIN_FILENO)) /* si mode interractif */
+		printf("$ "); /* affiche le prompt */
+
+	/* input utilisateur : saisie clavier si ineterractif sinon input standard */
+	while ((length = getline(&user_input, &bufsize, stdin)) != -1)
 	{
-		pid = fork();
-		if (pid == -1)
+		user_input[length - 1] = '\0'; /* remplace dernier caractère par \0*/
+		if (length > 1 && !est_vide(user_input))
 		{
-			perror("fork");
-			return (-1);
-		}
-		if (pid == 0)
-			execve(command, arguments, env);
-		else
-		{
-			wait(&status);
-			if (WIFEXITED(status) && WEXITSTATUS(status) == 2)
-				status = 2;
-		}
-		free(command);
-	}
-	else
-	{
-		status = 127;
-		fprintf(stderr, "%s: %d: %s: not found\n", argv[0],
-		*count_command, arguments[0]);
-	}
-	return (status);
-}
-/**
- * free_args - free arguments
- * @arguments: arguments
- *
- * Description: free memory
- * Return: Nothing
- */
-void free_args(char **arguments)
-{
-	int i;
+			count_cmd++; /*compte nb de fois qu'une commande est éxé (pour msg err) */
+			args = args_dans_tableau(user_input);/*créer tableau avec args de input*/
 
-	if (arguments != NULL)
-	{
-		for (i = 0; arguments[i] != NULL; i++)
-			free(arguments[i]);
-		free(arguments);
-	}
-}
-/**
- * *get_path - get a path where the command can be found
- * @ev: environment variables
- * @command: the command we are searching for
- *
- * Description: Take as input the command typed by the user and return a
- * string containing the path were the command can be found
- * Return: a pointer to the path or null (failure)
- */
-char *get_path(char *command, char **ev)
-{
-	char *path = NULL, *result = NULL, *token = NULL;
-	int i, result_len = 0;
-	struct stat file_info;
-
-	if (command[0] == '/' || (command[0] == '.'))
-		if (stat(command, &file_info) == 0) /*if the command start with '/' or '.'*/
-			return (strdup(command)); /*no need to search for a path*/
-
-	for (i = 0; ev[i]; i++) /*get path from environment*/
-		if (ev[i][0] == 'P' && ev[i][1] == 'A' && ev[i][2] == 'T' && ev[i][4] == '=')
-			path = strdup(ev[i]);
-
-	if (path != NULL)
-	{
-		token = strtok(path, "="); /*remove "PATH="*/
-		token = strtok(NULL, ":");
-		while (token != NULL) /*combine each path with the command*/
-		{
-			result_len = strlen(token) + strlen(command) + 2;
-			result = malloc(result_len);
-			for (i = 0; i < result_len; i++)
-				result[i] = 0;
-
-			strcat(result, token);
-			strcat(result, "/");
-			strcat(result, command);
-			if (stat(result, &file_info) == 0) /*test if the command exist here*/
+			if (strcmp(args[0], "exit") == 0)   /*si commande (args[0])= "exit" */
 			{
-				free(path);
-				return (result);
+				libererMemoireTab(args); /* Libérer la mémoire allouée au tab args  */
+				break;  /*quitte la boucle pour quitter le programme */
 			}
-			free(result);
-			result = NULL;
-			token = strtok(NULL, ":");
-		}
-	}
-	free(path);
-	return (0);
-}
-/**
- * **parse_command_line - function to get the executable
- * @command: pointer to the command line
- * @arguments: array of pointers to arguments
- *
- * Description: cut the command line into arguments and make an array with it
- * Return: NOTHING
- */
-char **parse_command_line(char *command, char **arguments)
-{
-	char *line = strdup(command), *arg;
-	int i = 0, count_args = 0;
-
-	arg = strtok(line, " \n");
-	while (arg != NULL) /* counts the number of arguments for malloc*/
-	{
-		arg = strtok(NULL, " \n");
-		count_args++;
-	} /* finished counting*/
-
-	free(line);
-	if (count_args == 0)
-		return (0);
-
-	arguments = realloc(arguments, sizeof(char *) * (count_args + 1));
-	if (arguments == NULL)
-		exit(EXIT_FAILURE);
-
-	arguments[count_args] = NULL;
-	line = strdup(command); /*go back to start*/
-
-	/* put arguments inside array arguments*/
-	arg = strtok(line, " \n");
-	while (arg != NULL)
-	{
-		arguments[i] = strdup(arg);
-		arg = strtok(NULL, " \n");
-		i++;
-	}
-	free(line);
-	return (arguments);
-}
-
-/**
- * main - function simple shell
- * @argc: argument count (not used)
- * @argv: argument values (not used)
- * @env: environ variables
- *
- * Descrpition: command interpretor
- * Return: int
- */
-int main(int argc, char **argv, char **env)
-{
-	size_t arg_count = 0;
-	int count_command = 0, status = 0, i;
-	char **arguments = NULL, *command_line = NULL, *full_path;
-
-	(void)argc;
-
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(1, "$ ", 2);
-		if (getline(&command_line, &arg_count, stdin) == -1)
-			break;
-		arguments = parse_command_line(command_line, arguments);
-		if (arguments != NULL)
-		{
-			count_command++;
-			i = 0;
-			if (strcmp(arguments[0], "env") == 0)
-				while (env[i]) /*print the environment*/
-					printf("%s\n", env[i++]);
-			else /*test exit*/
+			/*teste si args[0] existe dans env et complete arg[0] avec path si besoin*/
+			if (strcmp(args[0], "env") == 0)
+				_print_env(env);
+			else
 			{
-				if (strcmp(arguments[0], "exit") == 0)
-					break;
-				full_path = get_path(arguments[0], env);
-
-				status = exec_cmd(full_path, arguments, env, argv, &count_command);
+				cmd_trouv = _which(args, env);
+				/* Exceute commande et retourn code erreur le cas echeant*/
+				status = execute_command(cmd_trouv, args, env, count_cmd, av[0]);
 			}
-			free_args(arguments);
+			/* Libérer la mémoire allouée a args  */
+			libererMemoireTab(args);
 		}
-		arguments = NULL;
-	}
-	free_args(arguments);
-	free(command_line);
+		if (isatty(STDIN_FILENO)) /* si mode interractif */
+			printf("$ "); /* affiche le prompt */
+	} /*fin while*/
+	free(user_input); /* libere la mémore issue de la fonction getline */
 	return (status);
 }
